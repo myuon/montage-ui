5a615d41941240be2833db72885e649c2e18b18a
myuon <ioi.joi.koi.loi@gmail.com>
2025-06-14 20:39:51 +0900

    chore: stagewiseの依存関係とコードを削除

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { StagewiseToolbar } from "@stagewise/toolbar-react";
-import { ReactPlugin } from "@stagewise-plugins/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <StagewiseToolbar
-      config={{
-        plugins: [ReactPlugin],
-      }}
-    />
   </StrictMode>
 );
